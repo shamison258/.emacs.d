a4f352866a5589585e7dc86209a476c5e31371ea
shamison <unknown0incident@gmail.com>
2015-05-18 01:30:27 +0900

    c

---
+++ elpa/mozc-20140802.56/mozc-pkg.el
@@ -0,0 +1,1 @@
+(define-package "mozc" "20140802.56" "minor mode to input Japanese with Mozc" 'nil :keywords '("mule" "multilingual" "input method"))
--- init.el
+++ init.el
@@ -10,6 +10,8 @@
 ;; .#* とかのバックアップファイルを作らない
 (setq auto-save-default nil)
 
+;; スタートアップをひょうじさせない
+(setq inhibit-startup-message t)
 
 ;; auto-complete
 (require 'auto-complete)
@@ -24,6 +26,9 @@
 ;; 日本語
 (set-language-environment 'Japanese)
 
+;; mozc
+(require 'mozc)
+
 ;; UTF-8
 (set-default-coding-systems 'utf-8)
 (set-keyboard-coding-system 'utf-8)
@@ -31,10 +36,28 @@
 (set-buffer-file-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
 
+;; molokai
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(load-theme 'molokai t)
+
 ;; Font
 (set-face-attribute 'default nil
-		    :family "Ricty"
+		    :family "Ricty Regular"
 		    :height 180)
+(defconst FONT_SIZE 10)
 
 ;; 行番号表示
 (global-linum-mode t)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Ricty" :foundry "unknown" :slant normal :weight normal :height 98 :width normal)))))
