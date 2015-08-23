75c02014bcbf8ea1d421a4c4a0172612dab0b55b
shamison <unknown0incident@gmail.com>
2015-08-23 19:12:04 +0900

    update

--- init.el
+++ init.el
@@ -1,10 +1,60 @@
 ;; packageの設定
 (require 'package)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (add-to-list 'load-path "~/.emacs.d/package")
 (add-to-list 'load-path "~/.emacs.d/package/yatex")
 (package-initialize)
+
+
+;; --- エディタの設定 ---
+
+;; *.~ とかのバックアップファイルを作らない
+(setq make-backup-files nil)
+(setq delete-auto-save-files t)
+;; .#* とかのバックアップファイルを作らない
+(setq auto-save-default nil)
+
+;; 日本語
+(set-language-environment 'Japanese)
+
+;; UTF-8
+(set-default-coding-systems 'utf-8)
+(set-keyboard-coding-system 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(set-buffer-file-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
+;; Font
+(set-face-attribute 'default nil
+		    :family "Ricty")
+(defconst FONT_SIZE 11)
+
+;; tab幅
+(setq default-tab-width 2)
+;; タブの無効化
+(setq-default indent-tabs-mode nil)
+
+;; 行番号表示
+(global-linum-mode t)
+
+;; スクロールバー,ツールバー,メニューバー非表示
+(tool-bar-mode 0)
+(menu-bar-mode 0)
+(set-scroll-bar-mode nil)
+
+;; タイトルバーにファイルのフルパス表示
+(setq frame-title-format
+      (format "%%f - Emacs"))
+
+;; スタートアップをひょうじさせない
+(setq inhibit-startup-message t)
+
+;; 警告音を消す
+(setq visible-bell t)
+(setq ring-bell-function 'ignore)
 
 
 ;; --- プラグインの設定 ---
@@ -15,6 +65,18 @@
 (require 'auto-complete-config)
 (ac-config-default)
 (global-auto-complete-mode t)
+
+;; scheme-mode
+;; goshのエンコをutf-8に
+(setq process-coding-system-alist
+     (cons '("gosh" utf-8 . utf-8)
+           process-coding-system-alist))
+;; goshをrun-schemeで使う
+(setq scheme-program-name "gosh -i")
+;; schemeモードとrun-schemeモードにcmuschemeを使用
+(autoload 'scheme-mode "cmuscheme" "Major mode for Scheme." t)
+(autoload 'run-scheme "cmuscheme" "Run an inferior Scheme process." t)
+
 
 ;; markdown-mode
 (setq auto-mode-alist
@@ -30,11 +92,10 @@
 (add-to-list 'auto-mode-alist '("\\.prl$" . prolog-mode))
 (add-to-list 'auto-mode-alist '("\\.pro$" . prolog-mode))
 
-;; Clojure-mode
-(add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
-
 ;; Scala-mode
 (add-to-list 'auto-mode-alist '("\\.scala$" . scala-mode))
+;; Ensime
+(add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
 
 ;; 拡張子が .tex なら yatex-mode に
 (setq auto-mode-alist
@@ -44,19 +105,48 @@
 ;; twittering-mode-設定
 (setq twittering-timer-interval 60) ; 1分毎に自動更新
 
+;; --- Clojure 
+
+;; Clojure-mode
+;; (add-to-list 'auto-mode-alist '("\\.clj$" . clojure-mode))
+
+;; Ciderの設定
+(add-hook 'clojure-mode-hook 'cider-mode)
+;; mini bufferに関数の引数を表示させる
+(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
+;; 'C-x b' した時に *nrepl-connection*
+;; と *nrepl-server* のbufferを一覧に表示しない
+(setq nrepl-hide-special-buffers t)
+;; RELPのbuffer名を 'project名:nREPLのport番号' と表示する
+;; project名は project.clj で defproject した名前
+(setq nrepl-buffer-name-show-port t)
+;; C-c C-zでcider-replを出す
+(setq cider-repl-display-in-current-window t)
+;; prompt
+(setq cider-repl-prompt-abbreviated t)
+;; history file path
+(setq cider-repl-history-file "~/.emacs.d/cider-history")
+(setq cider-repl-use-pretty-printing t)
+
+;; ac-ciderの設定
+(autoload 'ac-cider "ac-cider" nil t)
+(add-hook 'cider-mode-hook 'ac-flyspell-workaround)
+(add-hook 'cider-mode-hook 'ac-cider-setup)
+(add-hook 'cider-repl-mode-hook 'ac-cider-setup)
+(eval-after-load "auto-complete"
+  '(progn
+     (add-to-list 'ac-modes 'cider-mode)
+     (add-to-list 'ac-modes 'cider-repl-mode)))
 
 ;; 鬼軍曹.el
 (require 'drill-instructor)
 (setq drill-instructor-global t)
 
-;; slack-el
-(defvar slack-token "xoxp-4347824992-4347824994-7676052052-4ff5c0")
-(defvar slack-username "shamison")
-
-
 ;; smartparens
 (require 'smartparens-config)
 (smartparens-global-mode t)
+;; 対応括弧をハイライト
+(show-paren-mode t)
 
 
 ;; rainbow-delimiters 
@@ -64,6 +154,7 @@
 (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
 (add-hook 'scheme-mode-hook 'rainbow-delimiters-mode)
 (add-hook 'lisp-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
 ;; 括弧の色を強調する設定
 (require 'cl-lib)
 (require 'color)
@@ -118,68 +209,10 @@
                         (substring input-pattern 1)
                       (concat ".*" input-pattern)))))))
 
-
-;; --- エディタの設定 ---
-
-
-;; *.~ とかのバックアップファイルを作らない
-(setq make-backup-files nil)
-(setq delete-auto-save-files t)
-;; .#* とかのバックアップファイルを作らない
-(setq auto-save-default nil)
-
-;; 日本語
-(set-language-environment 'Japanese)
-
-;; UTF-8
-(set-default-coding-systems 'utf-8)
-(set-keyboard-coding-system 'utf-8)
-(set-terminal-coding-system 'utf-8)
-(set-buffer-file-coding-system 'utf-8)
-(prefer-coding-system 'utf-8)
-
-;; Font
-(set-face-attribute 'default nil
-		    :family "Ricty")
-(defconst FONT_SIZE 11)
-
-;; tab幅
-(setq default-tab-width 2)
-;; タブの無効化
-(setq-default indent-tabs-mode nil)
-
-;; 行番号表示
-(global-linum-mode t)
-
-;; スクロールバー,ツールバー,メニューバー非表示
-(tool-bar-mode 0)
-(menu-bar-mode 0)
-(set-scroll-bar-mode nil)
-
-;; タイトルバーにファイルのフルパス表示
-(setq frame-title-format
-      (format "%%f - Emacs"))
-
-;; Theme loadinfg
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-(setq molokai-theme-kit t)
-(load-theme 'molokai t)
-
-
-
-;; スタートアップをひょうじさせない
-(setq inhibit-startup-message t)
-
-;; 警告音を消す
-(setq visible-bell t)
-(setq ring-bell-function 'ignore)
-
-;; 対応括弧をハイライト
-(show-paren-mode t)
-
-
-;; --- キーバインドの設定
-
-
-;; C-hをBackSpaceにする
+;; Theme load
+(if window-system
+    (progn
+      (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+      (setq molokai-theme-kit t)
+      (load-theme 'molokai t)))
 
