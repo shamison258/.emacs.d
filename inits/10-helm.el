bcef5bd0dc3b88a946eb48a7d0554fcabc765ea4
shamison <unknown0incident@gmail.com>
2015-11-08 02:33:24 +0900

    update

--- inits/10-helm.el
+++ inits/10-helm.el
@@ -5,18 +5,21 @@
 (define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
 (define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
 (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
+
 ;; minibufferでC-kをいい感じに使う設定
 (setq helm-delete-minibuffer-contents-from-point t)
 (defadvice helm-delete-minibuffer-contents
     (before helm-emulate-kill-line activate)
   "Emulate `kill-line' in helm minibuffer"
   (kill-new (buffer-substring (point) (field-end))))
+
 ;; ファイルが存在しない場合は何もしないように advice を定義
 (defadvice helm-ff-kill-or-find-buffer-fname
     (around execute-only-if-exist activate)
   "Execute command only if CANDIDATE exists"
   (when (file-exists-p candidate)
     ad-do-it))
+
 ;; 候補のフィルタリングをいい感じにする設定
 (defadvice helm-ff-transform-fname-for-completion
     (around my-transform activate)
